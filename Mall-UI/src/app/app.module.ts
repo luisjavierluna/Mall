dd1baaba1726dc9100401687128ffdfc72897ed6
luisjavierluna <javierlunaluis@gmail.com>
2022-10-06 19:16:19 -0500

    Added landing page component and used router outlet

--- Mall-UI/src/app/app.module.ts
+++ Mall-UI/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
