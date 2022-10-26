5d095b6e111c295c7acd77f1ab0c3623269f74f8
luisjavierluna <javierlunaluis@gmail.com>
2022-10-26 12:04:21 -0500

    Added the API project and its initial settings

---
+++ Mall-API/Mall-API/Mall-API/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mall_API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
